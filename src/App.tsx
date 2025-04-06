561844def0ccbcaac81b0cc59fc451bb9ee0c53f
Tal Apelfeld <talapelfeld123@gmail.com>
2025-04-06 22:09:19 +0300

    CV

--- src/App.tsx
+++ src/App.tsx
@@ -178,6 +178,25 @@ function App() {
                   </svg>
                 </a>
               </li>
+              <li>
+                <a href="/" download="CV.pdf">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    // strokeWidth={1.5}
+                    stroke="#2c3e50"
+                    width="50"
+                    height="50"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M7.5 7.5h-.75A2.25 2.25 0 0 0 4.5 9.75v7.5a2.25 2.25 0 0 0 2.25 2.25h7.5a2.25 2.25 0 0 0 2.25-2.25v-7.5a2.25 2.25 0 0 0-2.25-2.25h-.75m-6 3.75 3 3m0 0 3-3m-3 3V1.5m6 9h.75a2.25 2.25 0 0 1 2.25 2.25v7.5a2.25 2.25 0 0 1-2.25 2.25h-7.5a2.25 2.25 0 0 1-2.25-2.25v-.75"
+                    />
+                  </svg>
+                </a>
+              </li>
             </ul>
           </div>
         </div>
